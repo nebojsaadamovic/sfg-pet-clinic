51cdd782698bfd7e9d8144160735ed49fdebb2af
Nebojsa Adamovic <adamovic.nebojsa@gmail.com>
2020-07-12 23:12:20 +0200

    Added vets index page an controller ... closes #7

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,8 +1,11 @@
 package guru.springframework.sfgpetclinic;
 
+import org.junit.runner.RunWith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootApplication
 public class SfgPetClinicApplication {
 
