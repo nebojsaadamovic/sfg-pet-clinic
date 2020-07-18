b9dc882d55893f854bb65f8e8701558884a13f82
Nebojsa Adamovic <adamovic.nebojsa@gmail.com>
2020-07-18 05:55:54 +0200

    Add boostrap for data... closes #14

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/OwnerServiceMap.java
@@ -1,31 +1,40 @@
 package guru.springframework.sfgpetclinic.service.map;
 
 import guru.springframework.sfgpetclinic.model.Owner;
-import guru.springframework.sfgpetclinic.service.CrudService;
+import guru.springframework.sfgpetclinic.service.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner,Long > implements CrudService <Owner,Long>{
+public class OwnerServiceMap extends AbstractMapService<Owner,Long > implements OwnerService {
 
 
+    @Override
+    public Owner findByLastName(String lastName) {
+        return findByLastName(lastName);
+    }
+
+    @Override
     public Set<Owner> findAll() {
         return super.findAll();
     }
 
-    public Owner findById(Long id) {
-        return map.get(id);
-    }
-
-
+    @Override
     public void deleteById(Long id) {
         super.deleteById(id);
     }
 
+    @Override
     public void delete(Owner object) {
-        super.delete(object);
+     super.delete(object);
     }
 
+    @Override
     public Owner save(Owner object) {
         return super.save(object.getId(),object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
     }
 }
