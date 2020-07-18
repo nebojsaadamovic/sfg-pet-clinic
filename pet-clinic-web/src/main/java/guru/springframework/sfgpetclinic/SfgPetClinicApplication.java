ce643bff055b1123b20077b747aa8642e56c2f40
Nebojsa Adamovic <adamovic.nebojsa@gmail.com>
2020-07-19 01:48:40 +0200

    Added Owners list and update controler .  closes#16

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,11 +1,9 @@
 package guru.springframework.sfgpetclinic;
 
-import org.junit.runner.RunWith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootApplication
 public class SfgPetClinicApplication {
 
