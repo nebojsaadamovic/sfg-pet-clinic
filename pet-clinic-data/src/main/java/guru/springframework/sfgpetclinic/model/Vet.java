2e1ae3674de16551b896cece333cf01c3d9994a2
Nebojsa Adamovic <adamovic.nebojsa@gmail.com>
2020-09-09 23:57:22 +0200

    added specialities, addes to Vet .  closes #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,7 +1,17 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+    private Set<Speciality>getSpecialities;
 
+    public Set<Speciality> getGetSpecialities() {
+        return getSpecialities;
+    }
 
+    public void setGetSpecialities(Set<Speciality> getSpecialities) {
+        this.getSpecialities = getSpecialities;
+    }
 }
+
