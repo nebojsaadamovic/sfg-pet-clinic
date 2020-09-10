f7e8550c0b63f93a45ea80cee8851c242545f808
Nebojsa Adamovic <adamovic.nebojsa@gmail.com>
2020-09-10 23:06:36 +0200

    added PetTypeService.  closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/PetTypeService.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.service;
+
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+
+
+public interface PetTypeService  extends CrudService<PetType,Long>{
+
+
+}
+
